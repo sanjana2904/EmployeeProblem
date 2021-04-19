efce8b06ef4f38bcd68785221ca2e7051fa9684b
sanjana k <sanjanarthi@gmail.com>
2021-04-19 22:59:24 +0530

    Added UC7-Finding sum,avr,max,min

--- EmployeeProblem.sql
+++ EmployeeProblem.sql
@@ -20,3 +20,11 @@ select * from employee_payroll where start between '2021-01-01' and GETDATE()
 --UC6
 alter table employee_payroll add  gender varchar(6)
 update employee_payroll set gender = 'M' where name = 'bill' or name = 'charlie'
+
+--UC7
+
+select gender ,sum(salary) as maleOrFemale from employee_payroll group by gender
+select gender ,avg(salary) as maleOrFemale from employee_payroll group by gender
+select gender ,min(salary) as maleOrFemale from employee_payroll group by gender
+select gender ,max(salary) as maleOrFemale from employee_payroll group by gender
+ select gender ,count(salary) as maleOrFemale from employee_payroll group by gender
